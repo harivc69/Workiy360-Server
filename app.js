d7dc1871d6d36dfb389607658a6bc7b9594af020
Hari Krishnan <harivc64@gmail.com>
2025-06-18 10:48:44 +0530

    handler file setup

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const connectDB = require('./middlewares/database');
 const routes = require('./routers');
+const { default: serverlessExpress } = require('@vendia/serverless-express');
 
 const app = express();
 
@@ -16,4 +17,5 @@ app.get('/', (req, res) => {
   res.send('🚀 HRMS API is running');
 });
 
-module.exports = app;
+// Export Lambda handler
+exports.handler = serverlessExpress({ app });
